759d8083525c28751fbd6af2ea81d42334bfacd4
Thomas Nind <TZNind@dundee.ac.uk>
2019-05-13 15:13:17 +0100

    RDMPIMG-361 Added rake build scripts for CI

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using System.Reflection;
+
+[assembly: AssemblyCompany("Universities of Dundee and Edinburgh")]
+[assembly: AssemblyProduct("DicomTypeTranslation")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// These should be overwritten by release builds
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
