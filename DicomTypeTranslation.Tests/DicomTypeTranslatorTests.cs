1349cdb3e74d7d3533c6bcdc919decff6d71a973
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-06-18 11:36:02 +0100

    Move Bson tests to new class

--- DicomTypeTranslation.Tests/DicomTypeTranslatorTests.cs
+++ DicomTypeTranslation.Tests/DicomTypeTranslatorTests.cs
@@ -2,7 +2,6 @@
 using Dicom;
 using DicomTypeTranslation.Helpers;
 using DicomTypeTranslation.Tests.Helpers;
-using MongoDB.Bson;
 using NLog;
 using NUnit.Framework;
 using System;
@@ -114,126 +113,6 @@ namespace DicomTypeTranslation.Tests
         }
 
         [Test]
-        public void TestDicomBsonMappingRoundTrip_Simple()
-        {
-            var dataset = new DicomDataset
-            {
-                { DicomTag.SOPInstanceUID, "SOPInstanceUID-Test" }
-            };
-
-            BsonDocument document = DicomTypeTranslaterReader.BuildDatasetDocument(dataset);
-
-            Assert.NotNull(document);
-
-            DicomDataset reconstructedDataset = DicomTypeTranslaterWriter.BuildDatasetFromBsonDocument(document);
-
-            Assert.NotNull(reconstructedDataset);
-            Assert.True(DicomDatasetHelpers.ValueEquals(dataset, reconstructedDataset));
-        }
-
-        [Test]
-        public void TestConvertingEmptyDicomElements()
-        {
-            var dataset = new DicomDataset
-            {
-                new DicomFloatingPointSingle(DicomTag.SelectorFLValue),
-                new DicomApplicationEntity(DicomTag.SelectorAEValue)
-            };
-
-            BsonDocument document = DicomTypeTranslaterReader.BuildDatasetDocument(dataset);
-
-            Assert.True(document.Count() == 2);
-            Assert.True(document[0] == BsonNull.Value);
-            Assert.True(document[1] == BsonNull.Value);
-        }
-
-        [Test]
-        public void TestDicomBsonMappingRoundTrip_AllTypes()
-        {
-            // Change this to select a specific VR
-            DicomVR vrToTest = null;
-
-            foreach (DicomVR vr in TranslationTestHelpers.AllVrCodes)
-            {
-                if (vrToTest != null && vr != vrToTest)
-                    continue;
-
-                DicomDataset dataset = TranslationTestHelpers.BuildVrDataset(vr);
-
-                BsonDocument document = DicomTypeTranslaterReader.BuildDatasetDocument(dataset);
-                Assert.NotNull(document);
-
-                DicomDataset reconstructedDataset = DicomTypeTranslaterWriter.BuildDatasetFromBsonDocument(document);
-                Assert.NotNull(reconstructedDataset);
-
-                Assert.True(reconstructedDataset.Count() == 1);
-                Assert.True(DicomDatasetHelpers.ValueEquals(dataset, reconstructedDataset));
-            }
-        }
-
-        [Test]
-        public void TestBsonDocumentWithAllVRs()
-        {
-
-            BsonDocument document = DicomTypeTranslaterReader.BuildDatasetDocument(TranslationTestHelpers.BuildVrDataset());
-            Assert.NotNull(document);
-
-            Console.WriteLine("=== Bson Dataset ===\n" + TranslationTestHelpers.PrettyPrintBsonDocument(document) + "\n===\t===");
-        }
-
-        [Test]
-        public void TestTagWithMultipleVrs()
-        {
-            var usItem = new DicomUnsignedShort(DicomTag.GrayLookupTableDataRETIRED, 0, 1, ushort.MaxValue);
-            var owItem = new DicomOtherWord(DicomTag.GrayLookupTableDataRETIRED, 0, 1, ushort.MaxValue);
-
-            Assert.True(usItem.Tag.DictionaryEntry.ValueRepresentations.Length == 3);
-            Assert.True(owItem.Tag.DictionaryEntry.ValueRepresentations.Length == 3);
-
-            var usDataset = new DicomDataset { usItem };
-            var owDataset = new DicomDataset { owItem };
-
-            BsonDocument usDocument = DicomTypeTranslaterReader.BuildDatasetDocument(usDataset);
-            BsonDocument owDocument = DicomTypeTranslaterReader.BuildDatasetDocument(owDataset);
-
-            Assert.NotNull(usDocument);
-            Assert.NotNull(owDocument);
-
-            Console.WriteLine(TranslationTestHelpers.PrettyPrintBsonDocument(usDocument));
-            Console.WriteLine(TranslationTestHelpers.PrettyPrintBsonDocument(owDocument));
-
-            DicomDataset recoUsDataset = DicomTypeTranslaterWriter.BuildDatasetFromBsonDocument(usDocument);
-            DicomDataset recoOwDataset = DicomTypeTranslaterWriter.BuildDatasetFromBsonDocument(owDocument);
-
-            Assert.True(DicomDatasetHelpers.ValueEquals(usDataset, recoUsDataset));
-
-            //TODO This will fail. Although the tag and values are identical, the original is a DicomOtherWord element and the reconstructed one is a DicomUnsignedShort
-            //Note: Should be fixed by updates to the MongoDB (BSON) stuff
-            Assert.True(DicomDatasetHelpers.ValueEquals(owDataset, recoOwDataset));
-        }
-
-        [Test]
-        public void TestBothBsonTagFormats()
-        {
-            // We will store tags in MongoDB in either of the following formats, and have to handle both cases:
-            // (0123,4567)-TagDictionaryName
-            // TagDictionaryName
-
-            var ds = new DicomDataset
-            {
-                new DicomShortString(DicomTag.SelectorSHValue, "ShortStringValue")
-            };
-
-            BsonDocument bsonWithPrefix = DicomTypeTranslaterReader.BuildDatasetDocument(ds, true);
-            BsonDocument bsonWithoutPrefix = DicomTypeTranslaterReader.BuildDatasetDocument(ds, false);
-
-            DicomDataset recoDsFromPrefix = DicomTypeTranslaterWriter.BuildDatasetFromBsonDocument(bsonWithPrefix);
-            DicomDataset recoDsFromNoPrefix = DicomTypeTranslaterWriter.BuildDatasetFromBsonDocument(bsonWithoutPrefix);
-
-            Assert.True(DicomDatasetHelpers.ValueEquals(recoDsFromPrefix, recoDsFromNoPrefix));
-        }
-
-        [Test]
         public void TestSetDicomTagWithNullElement()
         {
             var dataset = new DicomDataset();
@@ -281,46 +160,6 @@ namespace DicomTypeTranslation.Tests
         }
 
         [Test]
-        public void TestBsonDatasetRebuild_WithPrivateTags_CreatorExists()
-        {
-            // Convert a dataset with private tag information (existing in the default dictionary) to bson
-            // Reconstruct and assert equality
-
-            var dataset = new DicomDataset
-            {
-                { DicomTag.SOPInstanceUID, "1.2.3.4" }
-            };
-
-            dataset.Add(new DicomCodeString(new DicomTag(1953, 16), "ELSCINT1"));
-            dataset.Add(new DicomUnsignedShort(new DicomTag(1953, 4176, "ELSCINT1"), 20));
-
-            BsonDocument bson = DicomTypeTranslaterReader.BuildDatasetDocument(dataset);
-            Assert.NotNull(bson);
-
-            DicomDataset recoDataset = DicomTypeTranslaterWriter.BuildDatasetFromBsonDocument(bson);
-
-            Assert.NotNull(recoDataset);
-            Assert.True(DicomDatasetHelpers.ValueEquals(dataset, recoDataset));
-        }
-
-        [Test]
-        public void TestBsonDatasetRebuild_WithPrivateTags_CreatorMissing()
-        {
-            var dataset = new DicomDataset
-            {
-                { DicomTag.SOPInstanceUID, "1.2.3.4" }
-            };
-
-            dataset.Add(new DicomCodeString(new DicomTag(1953, 16), "AAAHHHH"));
-            dataset.Add(new DicomUnsignedShort(new DicomTag(1953, 4176, "AAAHHHH"), 20));
-
-            BsonDocument bson = DicomTypeTranslaterReader.BuildDatasetDocument(dataset);
-            Assert.NotNull(bson);
-
-            Assert.Throws<ApplicationException>(() => DicomTypeTranslaterWriter.BuildDatasetFromBsonDocument(bson));
-        }
-
-        [Test]
         public void TestPatientAgeTag()
         {
             var dataset = new DicomDataset();
@@ -332,20 +171,6 @@ namespace DicomTypeTranslation.Tests
             Assert.AreEqual("009Y", cSharpValue);
         }
 
-        [Test]
-        public void TestMaxVrMultiplicity()
-        {
-            foreach (DicomDictionaryEntry entry in DicomDictionary.Default)
-            {
-                if (entry.ValueRepresentations.Length <= DicomTypeTranslaterWriter.MaxVrsToExpect)
-                    continue;
-
-                Console.WriteLine(entry.Name + " has " + entry.ValueRepresentations.Length +
-                                  " representations (" + string.Join(", ", entry.ValueRepresentations.Select(x => x.Name)) + ")");
-
-                Assert.Fail("Element has more value representations than we would expect");
-            }
-        }
 
         [Test]
         public void PrintValueTypesForVrs()
