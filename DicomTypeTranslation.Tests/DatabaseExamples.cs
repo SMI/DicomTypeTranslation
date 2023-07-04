a8aaf084784917b22dd21a59188291986029e390
James A Sutherland <>
2023-07-04 16:31:15 -0500

    Using

--- DicomTypeTranslation.Tests/DatabaseExamples.cs
+++ DicomTypeTranslation.Tests/DatabaseExamples.cs
@@ -37,7 +37,8 @@ public class DatabaseExamples : DatabaseTests
         tbl.AddColumn("FileLocation", new DatabaseTypeRequest(typeof(string), 500), true, 500);
 
         //Create a DataTable in memory for the data we read from disk
-        var dt = new DataTable();
+        using var dt = new DataTable();
+        dt.BeginDataLoad();
         dt.Columns.Add("SOPInstanceUID");
         dt.Columns.Add("Modality");
         dt.Columns.Add("PatientID");
@@ -61,6 +62,7 @@ public class DatabaseExamples : DatabaseTests
 
         //put the DataTable into the database
         using var insert = tbl.BeginBulkInsert();
+        dt.EndDataLoad();
         insert.Upload(dt);
 
 
